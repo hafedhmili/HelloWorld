dc244c98e5c45dc8f871ebcac25af40134462931
hafedhmili <mili.hafedh@uqam.ca>
2023-09-14 21:36:24 -0400

    added a println statement

--- src/ca/uqam/info/mgl7460/MainClass.java
+++ src/ca/uqam/info/mgl7460/MainClass.java
@@ -2,7 +2,10 @@ package ca.uqam.info.mgl7460;
 
 public class MainClass
 {
+    // https://gitlab.info.uqam.ca/mili_h/goldfitclient.git
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+
+        System.out.println("Not sure what the x stands for");
     }
 }
