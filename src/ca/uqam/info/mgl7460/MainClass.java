063b13e51e59b8225f3a0ce115d669e31ad47b87
hafedhmili <mili.hafedh@uqam.ca>
2023-09-19 16:25:22 -0400

    Cleaned MainClass.main()

--- src/ca/uqam/info/mgl7460/MainClass.java
+++ src/ca/uqam/info/mgl7460/MainClass.java
@@ -5,7 +5,5 @@ public class MainClass
     // https://gitlab.info.uqam.ca/mili_h/goldfitclient.git
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-
-        System.out.println("Not sure what the x stands for");
     }
 }
