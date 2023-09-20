fb782f3827ad342b58659ee5885ee12e708b8cf2
hafedhmili <mili.hafedh@uqam.ca>
2023-09-20 16:18:36 -0400

    1) remplacer Inscription de Record à Interface, parce qu'il s'est avéré que les champs d'un record sont non-immuables, 2) implanté les classes InscriptionImpl, EtudiantImpl, CoursImpl, et GroupeCoursImpl

--- src/ca/uqam/info/mgl7460/domain/Inscription.java
+++ src/ca/uqam/info/mgl7460/domain/Inscription.java
@@ -1,3 +1,3 @@
 package ca.uqam.info.mgl7460.domain;
 
-public record Inscription(GroupeCours gpCours, float noteNumerique, boolean reussi) {}
+public record Inscription(Etudiant etudiant, GroupeCours gpCours, float noteNumerique, boolean reussi) {}
